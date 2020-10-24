585c589200fdfd228a04149966f9d476e68d7b85
Kate <you@example.com>
2020-10-24 14:28:24 +0300

    views added

--- projectreviews/views.py
+++ projectreviews/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import  Projects,Profile
+from .serializer import ProjectsSerializer,ProfileSerializer
 
-# Create your views here.
+
+class PostListView(ListView):
+
