e368a89c0dfe31be6e4b16d672e5e3568236b62f
Kate <you@example.com>
2020-10-24 14:56:51 +0300

    tests

--- projectreviews/tests.py
+++ projectreviews/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 from .models import Projects,Profile,Review
 # Create your tests here.
+
+
+class ProjectsTest(TestCase):
+    def setUp(self):
+        self.description=Projects(location='description')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.description,Projects))
+
+    def tearDown(self):
+        Projects.objects.all().delete()
+
+    def test_save_method(self):
+        self.description.save_location()
+        projects = Projects.objects.all()
+        self.assertTrue(len(projects)>0)
+
+    def test_delete_method(self):
+        self.description.delete_location('description')
+        projects = Projects.objects.all()
+        self.assertTrue(len(projects)==0)
