be1af8c928d6288bfd53382188fb92700618365a
Kate <you@example.com>
2020-10-24 14:39:44 +0300

    model added

--- projectreviews/models.py
+++ projectreviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Profile(models.Model):
+    profile_picture=models.ImageField(upload_to='users/', default='user.png')
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    bio=models.TextField(default='Review')
+
+
+
+
+
