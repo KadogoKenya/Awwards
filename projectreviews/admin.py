581e632ffd8a2c413d80477c46ae5e6a33c5cb6b
Kate <you@example.com>
2020-10-25 10:56:03 +0300

    models registed at admin

--- projectreviews/admin.py
+++ projectreviews/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Projects,Profile
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Projects)
+admin.site.register(Project)
 admin.site.register(Review)
 
