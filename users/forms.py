8a036e2f84bee15857486fad4f6292c1b3f28be0
Kate <you@example.com>
2020-10-25 09:23:52 +0300

    user registration details added

--- users/forms.py
+++ users/forms.py
@@ -1,35 +1,24 @@
 from django import forms
-from django.forms import ModelForm
 from django.contrib.auth.models import User
-from projects.models import Projects,Profile
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-
-
-
-
-class UserRegisterForm(UserCreationForm):
-
-    email = forms.EmailField(label=" ",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}))
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email']
-
-class ProjectForm(forms.ModelForm):
-
-    class Meta:
-        model= Projects
-        exclude= ['author', 'created_date', 'author_profile']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email' ]
 
 class ProfileUpdateForm(forms.ModelForm):
+
     class Meta:
         model = Profile
-        fields = ['profile_picture']
+        fields = ['image', 'bio']
